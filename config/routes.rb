520f7a067c1087c73da0171becbf28e98a4fe140
Shanmukha <shanmukhakatta@gmail.com>
2015-10-13 14:51:21 +0530

    gemfile changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  root  'static_pages/contact'
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
